86d3c5bc1905ae070ad1104b550687b16fecadd9
Marcus Lo <marcuslo7773@gmail.com>
2025-09-04 15:05:10 +0800

    Add comment for drone cooldown functionality
    Added a comment explaining the purpose of the code.

--- cooldown.py
+++ cooldown.py
@@ -1,6 +1,8 @@
 from djitellopy import tello
 from time import sleep
 
+# Use this when the drone is overheating to cool it down.
+
 drone = tello.Tello(host='192.168.0.167')
 drone.connect()
 sleep(1)
